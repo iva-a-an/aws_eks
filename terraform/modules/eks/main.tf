d98d3549cb0303f73f380caed120022998e11b21
ross <ivanov.svc@gmail.com>
2025-06-06 14:38:49 +0800

    Add EBS support

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -141,7 +141,8 @@ resource "aws_eks_node_group" "spot" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "${var.env}-eks-spot-ng"
   node_role_arn   = aws_iam_role.eks_node_group.arn
-  instance_types  = var.env == "prod" ? ["t3.medium", "t3.large"] : ["t3.small", "t3.medium"]
+  instance_types  = var.env == "prod" ? ["t3.medium", "t3.large"] : ["t3.medium"]
+  capacity_type   = "SPOT"
   subnet_ids = [
     var.private_subnet_a_id,
     var.private_subnet_b_id
@@ -172,4 +173,52 @@ resource "aws_eks_node_group" "spot" {
     "kubernetes.io/cluster/${aws_eks_cluster.eks.name}" = "owned"
     "Name"                                              = "${var.env}-eks-spot-ng"
   })
+}
+
+
+# OIDC Provider for EKS
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+}
+
+
+data "aws_iam_policy_document" "csi" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "eks_ebs_csi_driver" {
+  assume_role_policy = data.aws_iam_policy_document.csi.json
+  name               = "eks-ebs-csi-driver"
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_ebs_csi_driver" {
+  role       = aws_iam_role.eks_ebs_csi_driver.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+# CSI Driver addon
+resource "aws_eks_addon" "csi_driver" {
+  cluster_name             = aws_eks_cluster.eks.name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.eks_ebs_csi_driver.arn
 }
--- terraform/modules/eks/versions.tf
+++ terraform/modules/eks/versions.tf
@@ -6,5 +6,9 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.33"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 }
